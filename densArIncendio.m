8ff189026e7bf577c4abb2774b2bbffe44ac8b7d
Carlos Kleber C Arruda <carloskleber@gmail.com>
2022-05-31 16:26:03 -0300

    alteracoes menores

--- densArIncendio.m
+++ densArIncendio.m
@@ -1,4 +1,4 @@
-function delta = densArIncendio(rho, h)
+function [t, delta] = densArIncendio(rho, h)
 %DENSARINCENDIO Densidade do ar corrigida para temperatura de incendio
 % rho - pressão atmosférica local em mmhg (padronizar pra hPa)
 % h - altura do cabo relativa a vegetacao em m
@@ -9,6 +9,7 @@ s = 0;
 for i = 1:h
   s = s + i^(-0.992);
 end
-delta = rho * t0 ./ (r0 * (273 + 976.94./h * s));
+t = 976.94./h * s;
+delta = rho * t0 ./ (r0 * (273 + t));
 end
 
